cd9ee708e03b93491cf25895c5099fe296c95a70
asyifa zahwa <azahwa201@gmail.com>
2024-12-01 23:15:09 +0700

    add file

---
+++ controllers/convertToDocx.go
@@ -0,0 +1,67 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/unidoc/unipdf/v3/common/license"
+	"github.com/unidoc/unipdf/v3/extractor"
+	"github.com/unioffice/unioffice/document"
+)
+
+// Pastikan untuk mengganti dengan lisensi UniPDF Anda
+const uniPDFLicense = "YOUR_UNIPDF_LICENSE_KEY"
+
+func ConvertToDocx() {
+	// Inisialisasi lisensi UniPDF
+	err := license.SetLicense(uniPDFLicense, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Membaca file PDF
+	filePath := "input.pdf"
+	pdfFile, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("Unable to open PDF file: %v", err)
+	}
+	defer pdfFile.Close()
+
+	// Ekstraksi teks dari PDF
+	pdfReader, err := unipdf.NewPdfReader(pdfFile)
+	if err != nil {
+		log.Fatalf("Unable to create PDF reader: %v", err)
+	}
+
+	// Membaca seluruh teks dari PDF
+	var extractedText string
+	for pageNum := 1; pageNum <= pdfReader.GetNumPages(); pageNum++ {
+		page, err := pdfReader.GetPage(pageNum)
+		if err != nil {
+			log.Fatalf("Unable to get page %d: %v", pageNum, err)
+		}
+
+		extractor := extractor.New(page)
+		text, err := extractor.ExtractText()
+		if err != nil {
+			log.Fatalf("Unable to extract text from page %d: %v", pageNum, err)
+		}
+		extractedText += text
+	}
+
+	// Buat file DOCX baru
+	doc := document.New()
+
+	// Tambahkan teks yang diekstraksi ke dalam dokumen DOCX
+	doc.AddParagraph().AddRun(extractedText)
+
+	// Simpan DOCX ke file
+	outputPath := "output.docx"
+	err = doc.SaveToFile(outputPath)
+	if err != nil {
+		log.Fatalf("Unable to save DOCX file: %v", err)
+	}
+
+	fmt.Println("PDF berhasil dikonversi ke DOCX:", outputPath)
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/bytedance/sonic/loader v0.2.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.7 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.10.0 // indirect
@@ -21,13 +22,23 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/unidoc/pkcs7 v0.2.0 // indirect
+	github.com/unidoc/timestamp v0.0.0-20200412005513-91597fd3793a // indirect
+	github.com/unidoc/unioffice v1.38.0 // indirect
+	github.com/unidoc/unipdf/v3 v3.64.0 // indirect
+	github.com/unidoc/unitype v0.4.0 // indirect
 	golang.org/x/arch v0.12.0 // indirect
 	golang.org/x/crypto v0.29.0 // indirect
+	golang.org/x/image v0.18.0 // indirect
 	golang.org/x/net v0.31.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
+	golang.org/x/xerrors v0.0.0-20240716161551-93cc26a95ae9 // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
